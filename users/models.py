e70c8a33e6bd575794c8a484de20924682e0e946
erdes10032 <erdes10032@gmail.com>
2026-05-21 15:46:17 +0300

    add users models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,68 @@
-from django.db import models
+from pathlib import Path
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .validators import validate_avatar
+
+
+def avatar_upload_path(instance, filename):
+
+    extension = Path(filename).suffix
+
+    return (
+        f'avatars/'
+        f'{instance.id}/'
+        f'avatar{extension}'
+    )
+
+
+class User(AbstractUser):
+
+    class Gender(models.TextChoices):
+
+        MALE = 'male', _('Male')
+
+        FEMALE = 'female', _('Female')
+
+    email = models.EmailField(
+        unique=True,
+        verbose_name=_('Email'),
+    )
+
+    gender = models.CharField(
+        max_length=10,
+        choices=Gender.choices,
+        default=Gender.MALE,
+        verbose_name=_('Gender'),
+    )
+
+    avatar = models.ImageField(
+        upload_to=avatar_upload_path,
+        validators=[validate_avatar],
+        blank=True,
+        null=True,
+        verbose_name=_('Avatar'),
+    )
+
+    bio = models.TextField(
+        blank=True,
+        verbose_name=_('Bio'),
+    )
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = [
+        'username',
+    ]
+
+    class Meta:
+
+        verbose_name = _('User')
+
+        verbose_name_plural = _('Users')
+
+    def __str__(self):
+
+        return self.email
