2f445b10c1a9b63419d77eaaefa7da854fdcf03d
erdes10032 <erdes10032@gmail.com>
2026-05-26 22:33:55 +0300

    update asgi | config

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,42 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
 
-from django.core.asgi import get_asgi_application
+from channels.auth import (
+    AuthMiddlewareStack,
+)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+from channels.routing import (
+    ProtocolTypeRouter,
+    URLRouter,
+)
 
-application = get_asgi_application()
+from django.core.asgi import (
+    get_asgi_application,
+)
+
+import debates.routing
+
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'config.settings',
+)
+
+
+django_asgi_app = (
+    get_asgi_application()
+)
+
+
+application = ProtocolTypeRouter(
+    {
+        'http': django_asgi_app,
+
+        'websocket': (
+            AuthMiddlewareStack(
+                URLRouter(
+                    debates.routing.websocket_urlpatterns
+                )
+            )
+        ),
+    }
+)
